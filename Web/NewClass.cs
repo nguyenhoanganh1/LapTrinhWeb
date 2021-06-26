81a3256ef565a6b3b0959366e684d2e732297c97
nguyenhoanganh1 <hoanganh25022000@gmail.com>
2021-06-26 23:29:38 +0700

    test

---
+++ Web/NewClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web
+{
+    public class NewClass
+    {
+        public string a { get; set; }
+    }
+}
