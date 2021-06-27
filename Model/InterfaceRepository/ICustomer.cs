84d60fa81defda1fc6f3faef563a4d12aa4d7257
nguyenhoanganh1 <hoanganh25022000@gmail.com>
2021-06-27 21:06:30 +0700

    Add to Cart , DAO , images product

---
+++ Model/InterfaceRepository/ICustomer.cs
@@ -0,0 +1,19 @@
+﻿using Model.EL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.IRepository
+{
+    public interface ICustomer
+    {
+        List<Customer> Customers();
+        Customer GetCustomerById(string Id);
+        void InsertCustomer(Customer customer);
+        void UpdateCustomer(Customer customer);
+        void DeleteCustomer(Customer customer);
+        void Save();
+    }
+}
---
+++ Model/InterfaceRepository/IProduct.cs
@@ -0,0 +1,19 @@
+﻿using Model.EL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.IRepository
+{
+    public interface IProduct
+    {
+        List<Product> Products();
+        Product GetProductById(int Id);
+        void InsertProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(Product product);
+        void Save();
+    }
+}
