8e2193291426382358d5c10114f57784e4f1b948
nguyenhoanganh1 <hoanganh25022000@gmail.com>
2021-06-27 09:47:56 +0700

    _Menu Layout

--- Web/App_Start/RouteConfig.cs
+++ Web/App_Start/RouteConfig.cs
@@ -20,9 +20,15 @@ namespace Web
              );
 
             routes.MapRoute(
+                name: "CTHD",
+                url: "chi-tiet-hoa-don",
+                defaults: new { controller = "Orderdetail", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Orderdetail", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
