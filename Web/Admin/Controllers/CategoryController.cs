89414a822e473956496b8f8c91d4500bab9b99bd
lê nhut <nhutle8420@gmail.com>
2021-06-27 21:16:03 +0700

    Thaydoi vitri

---
+++ Web/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        DataContext
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
