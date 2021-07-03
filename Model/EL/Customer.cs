d859e616d845faa607e84449c706d41ceb39d539
nhutle8420 <nhutle8420@gmail.com>
2021-07-03 14:19:26 +0700

    Thêm Thuộc tính databse

--- Model/EL/Customer.cs
+++ Model/EL/Customer.cs
@@ -1,11 +1,10 @@
-﻿namespace Model.EL
+namespace Model.EL
 {
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
-    using System.ComponentModel;
 
     public partial class Customer
     {
@@ -14,42 +13,32 @@
         {
             Orders = new HashSet<Order>();
         }
-        [Display(Name = "Tài Khoản: ")]
+
         [StringLength(20)]
         public string Id { get; set; }
 
-
-        [Required(ErrorMessage = "Xin Vui Lòng Nhập Mật Khẩu")]
-        [StringLength(maximumLength: 16, MinimumLength = 4, ErrorMessage = "Độ dài mật khẩu từ 4-16 kí tự")]
-        [Display(Name = "Mật khẩu: ")]
-        public string Password { get; set; }
-
-        [Required(ErrorMessage = "Xin Vui Lòng Nhập Tên Đầy Đủ Của Bạn")]
-
         [Required]
-        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "ID error.")]
         [StringLength(50)]
         public string Password { get; set; }
 
         [Required]
-        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Password Eror.")]
-
         [StringLength(50)]
-        [Display(Name ="Họ và Tên:")]
         public string Fullname { get; set; }
 
-        [Required(ErrorMessage = "Xin Vui Lòng Nhập Địa Chỉ Email ")]
+        [Required]
         [StringLength(50)]
         public string Email { get; set; }
 
         [Required]
-        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Email not a valid email.")]
         [StringLength(50)]
         public string Photo { get; set; }
 
         public bool Activated { get; set; }
 
         public bool Admin { get; set; }
+
+        [StringLength(100)]
+        public string ResetPasswordCode { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Order> Orders { get; set; }
--- Web/Controllers/UserController.cs
+++ Web/Controllers/UserController.cs
@@ -3,7 +3,10 @@ using Model.EL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using Web.Models;
 
@@ -12,6 +15,126 @@ namespace Web.Controllers
     public class UserController : Controller
     {
         DataContext db = new DataContext();
+        private object crypto;
+
+        public ActionResult FogotPassWord()
+        {
+            return View();
+        }
+        [NonAction]
+        public void SendVerificationLinkEmail(string emailID, string activationCode,string emailfor = "VerifyAccount")
+        {
+            var verifyUrl = "/User/"+emailfor+" + activationCode;
+            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, verifyUrl);
+
+            var fromEmail = new MailAddress("dotnetawesome@gmail.com", "Dotnet Awesome");
+            var toEmail = new MailAddress(emailID);
+            var fromEmailPassword = "********"; // Replace with actual password
+            string body = "";
+            string subject = "";
+            if (emailfor == "VerifyAccount")
+            {
+                subject = "Your account is successfully created!";
+                body = "<br/><br/>We are excited to tell you that your Dotnet Awesome account is" +
+               " successfully created. Please click on the below link to verify your account" +
+               " <br/><br/><a href='" + link + "'>" + link + "</a> ";
+            }
+            else
+            {
+                if (emailfor == "ResetPassword")
+                { 
+                    subject =" Reset Password"
+                    body = "Hi,<br/>br/>We got requet for reset your account password. please click on the below  link to reset your password"
+                        "<br/>br/><a href="+link+">Reset Password link</a>"
+                }
+            }
+
+
+
+
+            
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
+            };
+
+            using (var message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            })
+                smtp.Send(message);
+        }
+
+        [HttpPost]
+        public ActionResult FogotPassWord(String Email)
+        {
+            string message = "";
+            bool status = false;
+
+            var account = db.Customers.Where(a => a.Email == Email).FirstOrDefault();
+            if (account != null)
+            {
+                string ressetcode = Guid.NewGuid().ToString();
+                SendVerificationLinkEmail(account.Email, ressetcode, "ResetPassword");
+                account.ResetPassWordCode = ressetcode;
+/*                account.ResetPassWord = ressetcode;*/
+                db.Configuration.ValidateOnSaveEnabled = false;
+                db.SaveChanges();
+                message = "ResetPassword link has been sent to your email ID"
+            }
+            else { 
+                message = "Account not found"
+                ViewBag.Message = message;
+            }
+            return View();
+        }
+        public ActionResult ResetPassword(String Id)
+        {
+            var user = db.Customers.Where(a => a.Id == Id).FirstOrDefault();
+            if (user != null)
+            {
+                ResetPasswordModel model = new ResetPasswordModel();
+                model.ResetCode = Id;
+                return View(model);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+         public ActionResult ResetPassword(ResetPasswordModel model)
+        {
+            var message = "";
+            if (ModelState.IsValid)
+            {
+                var user = db.Customers.Where(a = > a.ResetPassCode == model.ResetCode).FirstOrDefault();
+                if (user != null)
+                {
+                    user.Password = Crypto.Hash(model.NewPassWord);
+                    user.ResetPasswordCode = "";
+                    db.Configuration.ValidateOnSaveEnabled = false;
+                    db.SaveChanges();
+                    message = "New Pass Word updata successfully"
+                }
+            }
+            else
+            {
+                message ="Something invalid"
+
+            }
+            ViewBag.Message = message;
+            return View(model);
+        }
         public ActionResult Dangky()
         {
             return View();
